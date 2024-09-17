acca0350b1e895b966b27218dc32ea2788dfc0cc
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-09-17 19:30:38 -0300

    ajustes jaca y función maxima
    se ajustas algunas lineas de codigo y se modifica la funcion de lista maxima

--- src/index.js
+++ src/index.js
@@ -11,21 +11,21 @@ function agregarUsuarios() {
         return;
     }
 
-    let usuarioExistente = usuarios.find(cuenta => cuenta.nombre === nombre && cuenta.billetera === billetera);
+    //let usuarioExistente = usuarios.find(cuenta => cuenta.nombre === nombre && cuenta.billetera === billetera);
 
-    if (usuarioExistente) {
-
+    /*if (usuarioExistente) {
         usuarioExistente.transaccion += transaccion;
         alert('Las transacciones han sido acumuladas.');
     } else {
-
+    */
         usuarios.push({ nombre, billetera, transaccion });
-        alert('Información guardada con éxito.');
-    }
+        //alert('Información guardada con éxito.');
+    //}
 
     document.getElementById('nombre').value = '';
     document.getElementById('billetera').value = '';
     document.getElementById('transaccion').value = '';
+    listaUsuarios();
 }
 
 function listaUsuarios() {
@@ -44,12 +44,23 @@ function billeteraMasTransacciones() {
     const transaccionMaximaLista = document.getElementById('transaccionMaximaLista');
     transaccionMaximaLista.innerHTML = ''; 
 
-    if (usuarios.length === 0) {
-        alert('No hay usuarios');
-        return;
-    }
+    if (usuarios.length === 0) return;
+    const transaccionesPorUsuario = {};
+    usuarios.forEach(function(transaccion){
+        if(!transaccionesPorUsuario[transaccion.nombre]){
+            transaccionesPorUsuario[transaccion.nombre]=new Set(); //Set evita duplicar nombres
+        }        
+    });
+    let resultado ='';
+    Object.entries(transaccionesPorUsuario).forEach(([nombre,billetera])=>{
+    const billeteraConMasTransacciones = Object.entries(billetera).reduce((prev,current)=>(prev[1]>current[1]?prev:current));
+    resultado += `${nombre}: ${billeteraConMasTransacciones[0]} (${billeteraConMasTransacciones[1]} transacciones)<br>`;
+    });
+    transaccionMaximaLista.innerHTML=resultado;
 
-    let transaccionMaxima = usuarios.reduce((prev, current) => (current.transaccion > prev.transaccion) ? current : prev);
 
-    transaccionMaximaLista.innerHTML = `<p>El usuario <strong>${transaccionMaxima.nombre}</strong> tiene la billetera <strong>${transaccionMaxima.billetera}</strong> con el mayor número de transacciones: <strong>${transaccionMaxima.transaccion}</strong>.</p>`;
+
+    //let transaccionMaxima = usuarios.reduce((prev, current) => (current.transaccion > prev.transaccion) ? current : prev);
+
+    //transaccionMaximaLista.innerHTML = `<p>El usuario <strong>${transaccionMaxima.nombre}</strong> tiene la billetera <strong>${transaccionMaxima.billetera}</strong> con el mayor número de transacciones: <strong>${transaccionMaxima.transaccion}</strong>.</p>`;
 }
